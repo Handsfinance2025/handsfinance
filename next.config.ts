00c69569f65080f04573607e1dbe193272e87d20
Handsfinance2025 <handsfinanceqi@gmail.com>
2025-05-30 14:09:12 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@ionic/react', '@stencil/core'], // Tambahkan paket lain jika perlu
+  // transpilePackages: ['@ionic/react', '@stencil/core'], // Tambahkan paket lain jika perlu
 };
   /* config options here */
 
